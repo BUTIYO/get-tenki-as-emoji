634b8103ed0f0b070beede30f5ba4dbbcd9573d2
BUTIYO <moriyamabutiyo@yahoo.co.jp>
2020-06-15 05:43:32 +0900

    update detail

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # get-tenki-as-emoji
+都市名を渡すと，その地の天気をemojiで返してくれる
+
+# example
+```shell
+$ python3 get.py Tokyo
+☀️
+```
--- get.py
+++ get.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-import requests
+import sys
 import json
+import requests
 
-city_name = "Tokyo"
-API_KEY = "xxx"
+API_KEY = "9bdbf57188df3149a85570cd59007f89"
 api = "http://api.openweathermap.org/data/2.5/weather?units=metric&q={city}&APPID={key}"
 
 def main():
     res = getData()
     weather_id = res["weather"][0]["id"]
-    print(weather_id)
+    # print(weather_id)
     emo = weahter_to_emoji(str(weather_id))
     print(emo)
 
 def getData():
-    url = api.format(city = city_name, key = API_KEY)
-    print(url)
+    url = api.format(city = sys.argv[1], key = API_KEY)
+    # print(url)
     response = requests.get(url)
     data = response.json()
     data = json.loads(response.text)
