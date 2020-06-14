4ef2a2889e88979830f8d28a537256425519929b
BUTIYO <moriyamabutiyo@yahoo.co.jp>
2020-06-15 05:51:32 +0900

    add get.py

--- get.py
+++ get.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-import requests
+import sys
 import json
+import requests
 
-city_name = "Tokyo"
+
 API_KEY = "xxx"
 api = "http://api.openweathermap.org/data/2.5/weather?units=metric&q={city}&APPID={key}"
 
 def main():
     res = getData()
     weather_id = res["weather"][0]["id"]
-    print(weather_id)
+    # print(weather_id)
     emo = weahter_to_emoji(str(weather_id))
     print(emo)
 
 def getData():
-    url = api.format(city = city_name, key = API_KEY)
-    print(url)
+    url = api.format(city = sys.argv[1], key = API_KEY)
     response = requests.get(url)
     data = response.json()
     data = json.loads(response.text)
